af16d03dc035c50635477727577d4b16042af536
aissahmed14-eng <aissahmed14@gmail.com>
2026-03-09 04:34:22 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-cr460-ahmed"
   location = "France Central"
 }
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-cr460-ahmed"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
