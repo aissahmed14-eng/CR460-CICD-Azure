13757bf4c315afbf5d816a76b69683939be6b2a2
aissahmed14-eng <aissahmed14@gmail.com>
2026-03-09 16:36:49 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-cr460"
+  name = "subnet-cr460-2"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
